0f51eb818f86295bec23e374d813c9ddc23b8a9c
Michal Paukszto <michal.paukszto@gmail.com>
2022-04-23 14:00:01 +0200

    add Secretary class

--- src/CompanyMain.java
+++ src/CompanyMain.java
@@ -1,11 +1,13 @@
 import company.Employee;
 import company.Manager;
+import company.Secretary;
 
 public class CompanyMain {
 
     public static void main(String[] args) {
         Employee employeeOne = new Employee("Jan", 200, 11);
         Manager employeeTwo = new Manager("Bogdan", 250, 50, 22);
+        Secretary employeeThree = new Secretary("Ala", 230, 33);
 
         employeeTwo.show();
     }
